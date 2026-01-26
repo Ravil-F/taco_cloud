a024bb5c75764b99e2f9fd294d44ac2e53b64d26
rf <rf_3088@mail.ru>
2026-01-26 20:59:05 +0300

    fix pom file, added testHomeController

--- src/main/java/taco_cloud/TacoCloudApplication.java
+++ src/main/java/taco_cloud/TacoCloudApplication.java
@@ -1,4 +1,4 @@
-package org.example.taco_cloud;
+package taco_cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TacoCloudApplication.class, args);
     }
 
